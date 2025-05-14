e938a0bbb22e4b8ec3d10c005e9e53acf033af58
Subhojit Sarkar <subhojit555666@gmail.com>
2025-05-15 00:59:43 +0530

    forgot password

--- app.js
+++ app.js
@@ -7,12 +7,33 @@ const routes = require('./routes');
 const methodOverride = require('method-override');
 const app = express();
 const PORT = process.env.PORT || 4000;
-
+const jwt = require('jsonwebtoken');
 // Middleware
 app.use(express.urlencoded({ extended: true }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+    if (token) {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await db.query(
+        'SELECT id, email, aadhaar_number FROM users WHERE id = $1',
+        [decoded.userId]
+      );
+      req.user = user.rows[0] || null;
+    }
+  } catch (err) {
+    console.error('Auth check error:', err);
+  }
+  next();
+});
 
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
+});
 // View engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -21,7 +42,7 @@ app.use(methodOverride('_method'));
 routes(app);
 
 // Home page
-app.get('/', (req, res) => res.render('home', { user: req.user || null }));
+app.get('/', (req, res) => res.render('home'));
 
 // Error handling
 app.use((err, req, res, next) => {
